d54c6d8088af89198ba74e7396a74ab0c0a2442d
erick <aguilar505088@gmail.com>
2020-11-05 16:42:02 -0600

    Login validado con el usuario admin

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,35 @@
+//constants menu
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('container-menu');
 const conMenu = document.getElementById('cont-menu');
-const cont = document.getElementById('pseudo-body')
-const body = document.body
+const cont = document.getElementById('pseudo-body');
+const body = document.body;
 
+//constants userWindow
+const btnUser = document.getElementById('btn-user')
+const userWindow = document.getElementById('user-window')
 
-let state = false;
-btnMenu.addEventListener('click', (e) => {
-    if (state === false) {
+//vars
+let stateMenu = false;
+let stateUser = false;
+
+//Events
+btnMenu.addEventListener('click', btnMenuPushed);
+btnUser.addEventListener('click', btnUserPushed);
+btnUser.addEventListener('click', btnUserPushed);
+
+//functions
+function btnMenuPushed(event) {
+    if (stateMenu === false) {
         menu.style.setProperty('opacity', `${1}`);
         menu.style.setProperty('visibility', 'visible');
         conMenu.style.setProperty('transform', `translateX(${0}%)`)
-
         cont.style.setProperty('width', `calc(${100}% - var(--menu-width))`)
-            // cont.style.setProperty('width', `${80}%`)
         cont.style.setProperty('float', `right`)
-
         body.style.setProperty('overflow-y', 'hidden')
         btnMenu.classList.remove('icon-menu')
         btnMenu.classList.add('icon-window-close')
-        state = true
+        stateMenu = true
     } else {
         menu.style.setProperty('opacity', `${0}`);
         menu.style.setProperty('visibility', 'hidden');
@@ -28,23 +38,17 @@ btnMenu.addEventListener('click', (e) => {
         body.style.setProperty('overflow-y', 'visible')
         btnMenu.classList.remove('icon-window-close')
         btnMenu.classList.add('icon-menu')
-
-        state = false
+        stateMenu = false
     }
-});
+}
 
-const btnUser = document.getElementById('btn-user')
-const userWindow = document.getElementById('user-window')
-
-let stateUser = false;
-btnUser.addEventListener('click', (e) => {
+function btnUserPushed(event) {
     if (stateUser === true) {
         userWindow.style.setProperty('opacity', `${1}`);
         userWindow.style.setProperty('visibility', 'visible');
         userWindow.style.setProperty('transform', `translateY(${0}%)`)
         btnUser.classList.remove('icon-down-dir')
         btnUser.classList.add('icon-up-dir')
-
         stateUser = false;
     } else {
         userWindow.style.setProperty('opacity', `${0}`);
@@ -52,7 +56,6 @@ btnUser.addEventListener('click', (e) => {
         userWindow.style.setProperty('transform', `translateY(${-100}%)`)
         btnUser.classList.add('icon-down-dir')
         btnUser.classList.remove('icon-up-dir')
-
         stateUser = true;
     }
-});
+}
